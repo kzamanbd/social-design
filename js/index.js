0434954d2a568fe84ed984613bdd9b5fe2ebf968
Shaun47 <shaunhossain655@gmail.com>
2022-10-17 22:16:27 +0600

    index page done

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+
+let searchForm = document.getElementsByClassName("search-form")[0];
+
+document.getElementById("search-btn").addEventListener("click",function(){
+    searchForm.classList.toggle("active");
+    
+})
